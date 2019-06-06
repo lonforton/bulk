d4dcb60ac563ec35af306c4c3444a3e6735149bc
eugene <lonforton@gmail.com>
2019-06-06 19:59:11 +0300

    Initial commit with .travis.yml

---
+++ unit_tests.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE allocator_test_module
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+
+BOOST_AUTO_TEST_SUITE(bulk_test_suite)
+
+BOOST_AUTO_TEST_CASE(bulk_output_test)
+{
+  BOOST_CHECK(1 == 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
