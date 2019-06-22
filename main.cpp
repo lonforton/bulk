f8f2f04357849d93a98056f8b0fa8478954cb785
eugene <lonforton@gmail.com>
2019-06-22 16:15:02 +0300

    Creation of display and file output classes and handling standard input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <memory>
+#include <boost/lexical_cast.hpp>
 
-int main()
+#include "bulk.h"
+
+int main(int argc, char *argv[])
 {
-  std::cout << "Hello" << std::endl;
+  std::cout << "Press q for quit" << std::endl;
+
+  if (argc < 2)
+  {
+    std::cout << "Not enough input args!" << std::endl;
+    return 1;
+  }
+
+  int size_of_block = boost::lexical_cast<int>(argv[1]);
+
+  if (!isdigit(size_of_block) && size_of_block <= 0)
+  {
+    std::cout << "Incorrect input args!" << std::endl;
+    return 1;
+  }
+
+  Publisher publisher(size_of_block);
+
+  auto file_output = std::make_unique<FileOutput>();  
+  auto display_output = std::make_unique<DisplayOutput>();   
+
+  publisher.subscribe(file_output.get());
+  publisher.subscribe(display_output.get());
+
+  std::string input_line;
+  while (std::getline(std::cin, input_line) && input_line != "q")
+  {
+    publisher.get_input(input_line);
+  }
 
   return 0;
 }
